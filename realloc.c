0f2f1f21751eec421b2ea5af1846a4cd7b84477d
Aanuoluwapo Adejimi <aanuoluwaff@gmail.com>
2022-11-16 03:39:22 -0800

    C simple shell project

--- realloc.c
+++ realloc.c
@@ -50,6 +50,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 
+	p = malloc(new_size);
+	if (!p)
+	return (NULL);
+
 	old_size = old_size < new_size ? old_size : new_size;
 	while (old_size--)
 		p[old_size] = ((char *)ptr)[old_size];
